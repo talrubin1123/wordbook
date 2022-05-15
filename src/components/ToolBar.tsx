fd4d394e7b9659f10558747de977dcd786d9e12f
talshuker <talshuker11@gmail.com>
2022-05-15 17:42:49 +0300

    feat: log out button

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,34 +1,50 @@
-import { IonHeader, IonPage, IonIcon, IonTitle, IonToolbar, IonButton, IonButtons, IonImg } from '@ionic/react';
-import { personCircle, ellipsisHorizontal, ellipsisVertical } from 'ionicons/icons';
+import { IonHeader, IonPage, IonIcon, IonContent, IonPopover, IonToolbar, IonButton, IonButtons, IonImg } from '@ionic/react';
+import { personCircle, ellipsisHorizontal, ellipsisVertical, logOutOutline } from 'ionicons/icons';
 import imgIcon from '../icon/icon.png';
 
 import './ToolBar.css';
-import Menu from './Menu';
 
-let disableMenu = true;
+import { menuController } from '@ionic/core';
+import { useState } from 'react';
 
-const handleOpenMenu = () => {
-  disableMenu = false;
+const handleOpenMenu = async () => {
+  if (!await menuController.isOpen('first')) {
+    menuController.toggle('left')
+    await menuController.enable(true, "first")
+    console.log(await menuController.open("first"))
+    console.log(await menuController.isEnabled())
+  }
 };
 
 const ToolBar: React.FC = () => {
+  const [showPopover, setShowPopover] = useState<{ open: boolean, event: Event | undefined }>({
+    open: false,
+    event: undefined,
+  });
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="secondary">
-            <IonButton>
-              <IonIcon slot="icon-only" icon={personCircle} />
+            <IonButton onClick={(e) => setShowPopover({ open: true, event: e.nativeEvent })}>
+              <IonIcon slot="icon-only" icon={personCircle} >
+                <IonPopover
+                  isOpen={showPopover.open}
+                  event={showPopover.event}
+                  onDidDismiss={e => setShowPopover({ open: false, event: undefined })}
+                >
+                  <IonButton>log out<IonIcon icon={logOutOutline} /></IonButton>
+                </IonPopover>
+              </IonIcon>
             </IonButton>
           </IonButtons>
           <IonButtons slot="primary">
-            <IonButton color="secondary" onClick={() => handleOpenMenu()}>
+            <IonButton color="secondary" onClick={async () => await handleOpenMenu()}>
               <IonIcon slot="icon-only" ios={ellipsisHorizontal} md={ellipsisVertical} />
             </IonButton>
           </IonButtons>
-          <IonImg src={imgIcon} slot="start" style={{ width : 100, height: 80 }}/>
+          <IonImg src={imgIcon} slot="start" style={{ width: 100, height: 80 }} />
         </IonToolbar>
-        <Menu/>
       </IonHeader>
     </IonPage>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,13 @@
 import { IonHeader, IonPage } from '@ionic/react';
 
 import ToolBar from '../components/ToolBar';
-import Menu from '../components/Menu';
 
 const MainPage: React.FC = () => {
     return (
       <IonPage>
-        <Menu/>
         <IonHeader>
         </IonHeader>
         <ToolBar />
-        
       </IonPage>
     );
   };
